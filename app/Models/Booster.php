1517c22aeaacb50ee5563fcaf24fe34c1678e4fd
Mazitov Atem <maz@2i.tusur.ru>
2024-08-10 20:12:43 +0700

    add model

---
+++ app/Models/Booster.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * TODO add foreign user + UUID
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $data
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Booster extends Model
+{
+    use HasFactory;
+}
