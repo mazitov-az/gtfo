5c64fe04e1f59ff917eab61674cfcf190990a34e
Mazitov Atem <maz@2i.tusur.ru>
2024-08-11 14:19:20 +0700

    install api

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\BoostersController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
