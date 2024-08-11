7f776e409bb780ad5f3dcf21508d3f063d892702
Mazitov Atem <maz@2i.tusur.ru>
2024-08-11 14:21:21 +0700

    add BoostersController

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/boosters', [BoostersController::class, 'export']);
---
+++ tests/Feature/Http/Controllers/BoostersControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http\Controllers;
+
+use Tests\TestCase;
+
+final class BoostersControllerTest extends TestCase
+{
+    public function test_export(): void
+    {
+        $response = $this->get('/api/boosters');
+
+        $response->assertStatus(200);
+    }
+}
